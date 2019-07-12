700d6e859960caa338ecc12b9ffa53906b2f4357
Dylan Reid <dgreid@chromium.org>
2019-07-12 11:10:15 -0700

    parsing a single member and default works

--- program_config/src/lib.rs
+++ program_config/src/lib.rs
@@ -5,7 +5,7 @@
 /// use program_config::create_config;
 /// create_config!((u32, foo, 2));
 /// fn main() {
-///     let c = Config { foo: 2u32 };
+///     let c = Config::default();
 ///     assert_eq!(c.foo, 2u32);
 /// }
 /// ```
@@ -17,18 +17,28 @@ use proc_macro2::{self, Punct, Spacing};
 use quote::quote;
 use quote::{ToTokens, TokenStreamExt};
 use syn::parse::{Parse, ParseStream};
-use syn::{parenthesized, parse_macro_input, token, Ident, Lit, Result, Token, Type};
+use syn::{parenthesized, parse_macro_input, token, Expr, Ident, Result, Token, Type};
 
 // The entire configuration space.
 struct ConfigStruct {
     items: Vec<ConfigItem>,
 }
 
+impl ConfigStruct {
+    pub fn members(&self) -> impl Iterator<Item = ItemDefinition> {
+        self.items.iter().map(|item| item.definition())
+    }
+
+    pub fn defaults(&self) -> impl Iterator<Item = ItemDefault> {
+        self.items.iter().map(|item| item.default())
+    }
+}
+
 // All the information about a particular configuration item.
 struct ConfigItem {
     var_type: Box<Type>,
     name: Ident,
-    default_val: Lit,
+    default_val: Expr,
 }
 
 impl ConfigItem {
@@ -36,6 +46,13 @@ impl ConfigItem {
         ItemDefinition {
             name: &self.name,
             var_type: &self.var_type,
+        }
+    }
+
+    pub fn default(&self) -> ItemDefault {
+        ItemDefault {
+            name: &self.name,
+            val: &self.default_val,
         }
     }
 }
@@ -52,6 +69,21 @@ impl<'a> ToTokens for ItemDefinition<'a> {
         self.name.to_tokens(tokens);
         tokens.append(Punct::new(':', Spacing::Joint));
         self.var_type.to_tokens(tokens);
+    }
+}
+
+// The default initailizer that will go in Default::default() for the configuration.
+// Used in a temporary list that can be iterated over and have to_tokens called.
+struct ItemDefault<'a> {
+    name: &'a Ident,
+    val: &'a Expr,
+}
+
+impl<'a> ToTokens for ItemDefault<'a> {
+    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+        self.name.to_tokens(tokens);
+        tokens.append(Punct::new(':', Spacing::Joint));
+        self.val.to_tokens(tokens);
     }
 }
 
@@ -86,11 +118,21 @@ impl Parse for ConfigStruct {
 pub fn create_config(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as ConfigStruct);
     // TODO - loop over all items.
-    let defs = input.items.iter().map(|item| item.definition());
+    let members = input.members();
+    let defaults = input.defaults();
     let struct_code = quote! {
         struct Config {
-            #(#defs),*
+            #(#members),*
         }
+
+        impl Default for Config {
+            fn default() -> Self {
+                Config {
+                   #(#defaults),*
+                }
+            }
+        }
+
     };
     struct_code.into()
 }
