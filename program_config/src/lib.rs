91f834be9cc19bd437386c310bb7a6c575898a1d
Dylan Reid <dgreid@chromium.org>
2019-07-12 11:07:48 -0700

    wip

--- program_config/src/lib.rs
+++ program_config/src/lib.rs
@@ -13,33 +13,61 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
+use proc_macro2::{self, Punct, Spacing};
 use quote::quote;
+use quote::{ToTokens, TokenStreamExt};
 use syn::parse::{Parse, ParseStream};
 use syn::{parenthesized, parse_macro_input, token, Ident, Lit, Result, Token, Type};
 
+// The entire configuration space.
+struct ConfigStruct {
+    items: Vec<ConfigItem>,
+}
+
+// All the information about a particular configuration item.
 struct ConfigItem {
-    mem_type: Box<Type>,
+    var_type: Box<Type>,
     name: Ident,
-    val: Lit,
+    default_val: Lit,
+}
+
+impl ConfigItem {
+    pub fn definition(&self) -> ItemDefinition {
+        ItemDefinition {
+            name: &self.name,
+            var_type: &self.var_type,
+        }
+    }
+}
+
+// The definition that will go in the struct.
+// Used in a temporary list that can be iterated over and have to_tokens called.
+struct ItemDefinition<'a> {
+    name: &'a Ident,
+    var_type: &'a Box<Type>,
+}
+
+impl<'a> ToTokens for ItemDefinition<'a> {
+    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+        self.name.to_tokens(tokens);
+        tokens.append(Punct::new(':', Spacing::Joint));
+        self.var_type.to_tokens(tokens);
+    }
 }
 
 impl Parse for ConfigItem {
     fn parse(input: ParseStream) -> Result<Self> {
-        let mem_type = input.parse()?;
+        let var_type = input.parse()?;
         let _: Token![,] = input.parse()?;
         let name = input.parse()?;
         let _: Token![,] = input.parse()?;
-        let val = input.parse()?;
+        let default_val = input.parse()?;
         Ok(ConfigItem {
-            mem_type,
+            var_type,
             name,
-            val,
+            default_val,
         })
     }
-}
-
-struct ConfigStruct {
-    items: Vec<ConfigItem>,
 }
 
 impl Parse for ConfigStruct {
@@ -58,10 +86,10 @@ impl Parse for ConfigStruct {
 pub fn create_config(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as ConfigStruct);
     // TODO - loop over all items.
-    let name = input.items[0].name.clone();
+    let defs = input.items.iter().map(|item| item.definition());
     let struct_code = quote! {
         struct Config {
-            #name: u32,
+            #(#defs),*
         }
     };
     struct_code.into()
