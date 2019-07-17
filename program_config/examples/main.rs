4e46135bb9ec078893ba976f9315d09733a567d8
Dylan Reid <dgreid@chromium.org>
2019-07-16 19:22:37 -0700

    format

--- program_config/examples/main.rs
+++ program_config/examples/main.rs
@@ -1,10 +1,16 @@
 extern crate program_config;
 use program_config::create_config;
 create_config!(
-    (NAME: foo, TYPE: u32, DEFAULT: 2, SET: |value: &str| {
+    (NAME: foo,
+     TYPE: u32,
+     DEFAULT: 2,
+     SET: |value: &str| {
         value.parse().unwrap()
     }),
-    (NAME: bar, TYPE: bool, DEFAULT: Default::default(), SET: |value: &str| {
+    (NAME: bar,
+     TYPE: bool,
+     DEFAULT: Default::default(),
+     SET: |value: &str| {
         if value.len() > 3 {
             true
         } else {
